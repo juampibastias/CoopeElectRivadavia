e833c200adc308bd97badb8b09761bdce033c26e
kiquino <kiquino@gmail.com>
2022-07-17 19:56:16 -0300

    tarifa social

--- src/pages/responsabilidadSocial.js
+++ src/pages/responsabilidadSocial.js
@@ -5,7 +5,8 @@ const ResponsabilidadSocial =()=>{
     return(
         <div className="contenedor-responsabilidadsocial">
             <div className="contenedor-banner">
-              <BannerTitle direccionimagen={"url(/TarifaSocial2.jpg)"} titulo={"Tarifa Social"}/>
+              <BannerTitle direccionimagen={"url(/banner2.png)"} titulo={"Tarifa Social"}/>
+              <div className="responsabilidad-content">
               <h3>¿Quiénes acceden a la Tarifa Social?</h3>
               <p>Acceden a la Tarifa Social titulares de servicios de gas natural y electricidad que sean:</p>
               <ul>
@@ -24,6 +25,7 @@ const ResponsabilidadSocial =()=>{
               <p>¿Cómo me puedo dar de alta para ser beneficiario de la Tarifa Social?</p>
               <p>A través de internet en el sitio web <a href="www.minem.gob.ar/formulario">www.minem.gob.ar/formulario</a> ó en la línea gratuita <a href="tel:0800 333 2182">0800 333 2182</a></p>
               <a href="www.epreweb.com.ar/tarifasocial">www.epreweb.com.ar/tarifasocial</a>
+              </div>
             </div>
         </div>
     )
